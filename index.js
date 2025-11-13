8f86328dea9af81d18c148df97f6ade4764d32ea
Shaifuzzman-Ratul <shaifuzzmanratul@gmail.com>
2025-11-13 23:40:18 +0600

    add get api for all products

--- index.js
+++ index.js
@@ -11,6 +11,9 @@ app.use(express.json());
 
 const uri = process.env.MONGO_URI;
 
+
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
